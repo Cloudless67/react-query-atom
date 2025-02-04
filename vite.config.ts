431269218c7369f70c3dd74c5cb29742639c526e
Cloudless67 <cloudlesslakitu67@gmail.com>
2025-02-04 20:30:02 +0900

    build: fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [dts({ tsconfigPath: "./tsconfig.app.json" })],
   build: {
     lib: {
       entry: "src/index.ts",
